9d4dc2e6df3f8120ba2cee33616224b2289a7fe3
H4rigovind <harigovindrajan11@gmail.com>
2025-02-21 23:41:50 +0530

    frontend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PnfComponent } from './pnf/pnf.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',component:HomeComponent
+    },
+    {
+        path:'about',component:AboutComponent
+    },
+    {
+        path:'contact',component:ContactComponent
+    },
+    {
+        path:'footer',component:FooterComponent
+    },
+    {
+        path:'header',component:HeaderComponent
+    },
+    {
+        path:'blog',component:BlogComponent
+    },
+    {
+        path:'gallery',component:GalleryComponent
+    },
+    {
+        path:'login',component:LoginComponent
+    },
+    {
+        path:'register',component:RegisterComponent
+    },
+    {
+        path:'**',component:PnfComponent
+    }
+];
